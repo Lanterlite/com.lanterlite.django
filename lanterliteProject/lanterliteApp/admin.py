38619dfc148cba41ec5f3e9ac5ae14f0317fcbea
Ifan Ramadhani <ifan.ra@outlook.co.id>
2017-07-04 22:24:29 +0700

    add item (Article)

--- lanterliteProject/lanterliteApp/admin.py
+++ lanterliteProject/lanterliteApp/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from lanterliteApp import models
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(models.Article)
--- lanterliteProject/lanterliteApp/models.py
+++ lanterliteProject/lanterliteApp/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Reporter(models.Model):
+	full_name = models.CharField(max_length=70)
+	
+	def _unicode_(self):
+		return self.full_name
+		
+class Article(models.Model):
+	pub_date = models.DateField()
+	headline = models.CharField(max_length=200)
+	content = models.TextField()
+	reporter = models.ForeignKey(Reporter)
+	
+	def _unicode_(self):
+		return self.headline
+		
